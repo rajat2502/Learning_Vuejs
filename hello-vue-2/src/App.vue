cf32122dac82d8adba188731ab97c9ce94d04bfd
Rajat Verma <rajatverma5885045@gmail.com>
2021-06-07 21:50:54 +0530

    learn prop and proptypes

---
+++ hello-vue-2/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <Greet name="Rajat" heroName="Batman" />
+  <Greet name="Diana" heroName="Spiderman" />
+  <Greet :name="name" :heroName="heroName" /> -->
+
+  <Article title="Hello World" :likes="50" :published="true" />
+</template>
+
+<script>
+// import Greet from './components/Greet.vue';
+import Article from './components/Article.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: 'Keshav',
+      heroName: 'Motu',
+    };
+  },
+  components: { Article },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-vue-2/src/components/Article.vue
@@ -0,0 +1,23 @@
+<template>
+  Article Component
+  <h2>{{ title }}</h2>
+  <h2>Likes: {{ likes }}</h2>
+  <h2>Published: {{ published ? 'YES' : 'NO' }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: 'Article Title',
+    },
+    likes: Number,
+    published: Boolean,
+  },
+};
+</script>
+
+<style scoped></style>
