76d2884f2caf11bb1c87343cb8be2a7722ea6fa5
Rajat Verma <rajatverma5885045@gmail.com>
2021-06-09 17:50:38 +0530

    learn http request in vue

--- hello-vue-3/src/App.vue
+++ hello-vue-3/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PostList />
+  <CreatePost />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostList from './components/PostList.vue';
+import CreatePost from './components/CreatePost.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { PostList, CreatePost },
+};
 </script>
 
 <style>
---
+++ hello-vue-3/src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template
+  ><h3>PostList Component</h3>
+  <button @click="getPosts">Load Post</button>
+
+  <h3 v-if="errorMsg">{{ errorMsg }}</h3>
+  <div v-for="post in posts" :key="post.id">
+    <h3>{{ post.id }}. {{ post.title }}</h3>
+    <p>{{ post.body }}</p>
+    <hr />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'PostList',
+  data() {
+    return {
+      posts: [],
+      errorMsg: '',
+    };
+  },
+  methods: {
+    getPosts() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => (this.posts = res.data))
+        .catch((err) => (this.errorMsg = 'Error retriving data'));
+    },
+  },
+};
+</script>
+
+<style scoped></style>
